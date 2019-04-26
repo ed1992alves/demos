e9473c3a793cb9ca67b8eee8a25ea3c2303d47bb
Edgar <edgar.costa@hitachivantara.com>
2019-04-26 16:05:12 +0100

    Fix navigation

---
+++ kpis/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" >
+    <div class="container">
+      <object :data="image1"></object>
+      <div class="tryme" @mouseup="forceUpdate">Click here</div>
+      <kpi ref="kpi1" label="Avg. Service Time" initial_info_icon="positive" initial_info_value="8.85" initial_info_units="MS" relative ="positive" relative_value="10%"></kpi>
+      <kpi ref="kpi2" label="Total IOPS" initial_info_icon ="negative" initial_info_value="113,277" initial_info_units="" relative ="negative" relative_value="0.15%"></kpi>
+      <object :data="image2"></object>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import kpi from './components/kpi.vue';
+import image1 from "./assets/images/background1.svg";
+import image2 from "./assets/images/background2.svg";
+
+
+export default {
+  data:  function(){
+    return {
+      image1: image1,
+      image2: image2,
+    }
+  },
+  name: 'app',
+  components: {kpi},
+  methods:{
+
+    forceUpdate: function(){
+      console.log("Entrei");
+      this.$refs.kpi1.forceUpdate();
+    }
+
+  }
+}
+
+</script>
+
+<style lang="scss">
+@import "./assets/styles/main.scss";
+ </style>
---
+++ kpis/src/components/kpi.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="kpi" >
+      <span class="title">{{label}}</span>
+      <div class="info">
+        <span class="icon" :class="initial_info_icon"></span>
+        <span class="value">{{initial_info_value}}</span>
+        <span class="units">{{initial_info_units}}</span>
+      </div>
+      <div class="relative" :class="relative">
+          <span class="icon"></span>
+           <span class="value">{{relative_value}}</span>
+           <span class="units">vs last 24 hours</span>
+      </div>
+    
+
+
+    
+
+  </div>
+</template>
+
+<script>
+
+export default {
+ data: function () {
+            return {
+                
+            }
+         },
+  props: ['label', 'initial_info_icon', 'initial_info_value', 'initial_info_units', 'relative', 'relative_value', 'relative_units'],
+
+  mounted: function(){
+
+    this.forceUpdate();
+    
+    
+  },
+  methods: {
+      forceUpdate: function(){
+
+        $('#kpi .icon').addClass('loading');  
+        $('#kpi .value').addClass('loading');
+        $('#kpi .units').addClass('loading');
+
+          setTimeout(function(){ 
+          $('.info .icon').removeClass('loading');  
+          $('.info .value').removeClass('loading');
+          $('.info .units').removeClass('loading');
+
+          setTimeout(function(){ 
+            $('.relative .icon').removeClass('loading');  
+            $('.relative .value').removeClass('loading');
+            $('.relative .units').removeClass('loading');
+          }, 0);
+
+        }, 200);
+      }
+    }
+}
+
+</script>
+
+<style lang="scss">
+ </style>
